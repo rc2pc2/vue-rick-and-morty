0ea57dea6dc5f76fd7bcef6397c46983872ce99d
rc2pc2 <petriccarcc@gmail.com>
2023-06-19 12:35:21 +0200

    small refactoring on components and base scss

--- src/components/CharacterCard.vue
+++ src/components/CharacterCard.vue
@@ -11,6 +11,7 @@
         </div>
     </article>
 </template>
+
 <script>
 export default {
     name:'CharacterCard',
@@ -27,6 +28,7 @@ export default {
     }
 }
 </script>
+
 <style lang="scss">
     article{
         box-shadow: rgba(0, 0, 0, 0.25) 0px 54px 55px, rgba(0, 0, 0, 0.12) 0px -12px 30px, rgba(0, 0, 0, 0.12) 0px 4px 6px, rgba(0, 0, 0, 0.17) 0px 12px 13px, rgba(0, 0, 0, 0.09) 0px -3px 5px;
